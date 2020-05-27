c3dbd42f018d02e5c1ce3ce5c954d06f5aea9f81
Daryll Capistrano <daryllcapistrano@gmail.com>
2020-05-26 22:27:41 -0700

    add partial game logic to condiationally render score display

--- src/index.js
+++ src/index.js
@@ -1,64 +1,36 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-const Display = ({ counter }) => <div>{counter}</div>;
-
-const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button>;
-
-const DirectionButton = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
-
-const History = (props) => {
-	if (props.allClicks.length === 0) {
-		return <div>the app is used by pressing the buttons</div>;
+const Display = (props) => {
+	if (props.score === 5) {
+		return <div>game over</div>;
 	} else {
-		return <div>button press history: {props.allClicks.join(' ')}</div>;
+		return <div>{props.score}</div>;
 	}
 };
 
+const Button = (props) => {
+	return <button onClick={props.handleClick}>{props.text}</button>;
+};
+
 const App = () => {
-	const [ counter, setCounter ] = useState(0);
-	const [ left, setLeft ] = useState(0);
-	const [ right, setRight ] = useState(0);
-	const [ allClicks, setAll ] = useState([]);
-	// log the counter state
-	console.log('rendering...', counter);
+	const [ score, setCounter ] = useState(0);
 
-	// const [ clicks, setClicks ] = useState({
-	// 	left: 0,
-	// 	right: 0
-	// });
-
-	const handleLeftClick = () => {
-		setAll(allClicks.concat('L'));
-		setLeft(left + 1);
+	const incrementCounter = () => {
+		setCounter(score + 1);
+		if (score === 5) {
+			setCounter(0);
+		}
 	};
-
-	const handleRightClick = () => {
-		setAll(allClicks.concat('R'));
-		setRight(right + 1);
-	};
-
-	const incrementCounter = () => setCounter(counter + 1);
-	const decrementCounter = () => setCounter(counter - 1);
-	const resetCounter = () => setCounter(0);
 
 	return (
 		<div>
-			<Display counter={counter} />
-			<Button handleClick={incrementCounter} text="plus" />
-			<Button handleClick={decrementCounter} text="minus" />
-			<Button handleClick={resetCounter} text="reset" />
-			<br />
-			<br />
-			<div>
-				<div>
-					{left}
-					<DirectionButton onClick={handleLeftClick} text="left" />
-					<DirectionButton onClick={handleRightClick} text="right" />
-					{right}
-					<History allClicks={allClicks} />
-				</div>
-			</div>
+			<Display score={score} />
+			<Button handleClick={incrementCounter} text="S" />
+			<Button handleClick={incrementCounter} text="K" />
+			<Button handleClick={incrementCounter} text="A" />
+			<Button handleClick={incrementCounter} text="T" />
+			<Button handleClick={incrementCounter} text="E" />
 		</div>
 	);
 };
