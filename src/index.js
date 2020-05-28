0cecfe66f62e2ab964ed3bd8c708ddfaa8347933
Daryll Capistrano <daryllcapistrano@gmail.com>
2020-05-27 20:58:51 -0700

    expand game logic and fix buttons breaking app

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
+const Greeting = (props) => {
+	if (props.total === 0) {
+		return <div>Start Game</div>;
+	} else {
+		return <div> </div>;
+	}
+};
+
 const ScorekeeperDisplay = (props) => {
 	if (props.total === 0) {
-		return <div>begin new game</div>;
+		return <div>no score</div>;
 	}
 	if (props.total === 1) {
 		return (
@@ -44,18 +52,31 @@ const ScorekeeperDisplay = (props) => {
 };
 
 const Button = (props) => {
-	return <button onClick={props.handleClick}>{props.text}</button>;
+	return (
+		<button onClick={props.handleClick} disabled={props.disabled}>
+			{props.text}
+		</button>
+	);
 };
 
 const App = () => {
 	const [ total, setTotal ] = useState(0);
+	const isEnabled = total === 5;
 
 	const resetGame = () => {
 		setTotal(0);
 	};
 
+	const removeLetter = () => {
+		if (total === 0) {
+			return null;
+		} else {
+			setTotal(total - 1);
+		}
+	};
+
 	const landedCounter = () => {
-		console.log(total);
+		return null;
 		// do nothing or pass to next player
 	};
 
@@ -65,9 +86,11 @@ const App = () => {
 
 	return (
 		<div>
+			<Greeting total={total} />
 			<ScorekeeperDisplay total={total} />
-			<Button handleClick={landedCounter} text="Landed It" />
-			<Button handleClick={missedCounter} text="Missed It" />
+			<Button handleClick={landedCounter} disabled={isEnabled} text="Landed" />
+			<Button handleClick={missedCounter} disabled={isEnabled} text="Missed" />
+			<Button handleClick={removeLetter} disabled={isEnabled} text="Remove A Letter" />
 			<Button handleClick={resetGame} text="Start New Game" />
 		</div>
 	);
